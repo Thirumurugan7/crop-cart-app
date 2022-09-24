17dcbb6096717186c63b6ce194b40c4d9423a37a
Thirumurugan <thiru82003@gmail.com>
2022-09-24 13:33:31 +0530

    fc

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,13 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Get Agri-Products delivered on time</h2>
+      <p>Seeds to Crops</p>
+      <p>Get it delivered to your home with ease</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
